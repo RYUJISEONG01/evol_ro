639fa30e8bd15abfe15b06814584a47a0ea6539a
RYUJISEONG01 <wsd2380@naver.com>
2025-01-15 17:55:46 +0900

    변경

--- generate.py
+++ generate.py
@@ -4,9 +4,9 @@ def create_robot():
     pyrosim.Start_URDF("body.urdf")
 
     pyrosim.Send_Cube(name="Torso",pos=[0, 0, 0.5],size=[1, 1, 1])
-    pyrosim.Send_Joint(name="Torso_R_Leg",parent="Torso", child="R_Leg",type="revolute",position=[0.5, 0, 1.5])
-    pyrosim.Send_Cube(name="R_Leg",pos=[0.5, 0, 0],size=[1, 1, 1])
-    pyrosim.Send_Joint(name="Torso_L_Leg",parent="Torso",child="L_Leg",type="revolute",position=[-0.5, 0, 1.5])
-    pyrosim.Send_Cube(name="L_Leg",pos=[-0.5, 0, 0],size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_RLeg",parent="Torso", child="RLeg",type="revolute",position=[0.5, 0, 1.5])
+    pyrosim.Send_Cube(name="RLeg",pos=[0.5, 0, 0],size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_LLeg",parent="Torso",child="LLeg",type="revolute",position=[-0.5, 0, 1.5])
+    pyrosim.Send_Cube(name="LLeg",pos=[-0.5, 0, 0],size=[1, 1, 1])
     pyrosim.End()
 create_robot()
--- simulation.py
+++ simulation.py
@@ -1,14 +1,19 @@
 import pybullet as p
+import pyrosim.pyrosim as pyrosim
 import pybullet_data
 physicsclient =p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 p.loadURDF("plane.urdf")
-planeId=p.loadURDF("body.urdf")
+robotId=p.loadURDF("body.urdf")
+
+pyrosim.Prepare_To_Simulate(robotId)
 
 import time
 for i in range(1000) :
     p.stepSimulation()
+    LLeg_touchsensor=pyrosim.Get_Touch_Sensor_Value_For_Link("LLeg")
+    print(f"sensor value :{LLeg_touchsensor}"  )
     time.sleep(1/60)
     print(i)
 p.disconnect()
