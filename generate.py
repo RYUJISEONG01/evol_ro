f4022ddf80f044f5cb989385c2523dabbb1b9a38
RYUJISEONG01 <wsd2380@naver.com>
2025-01-15 16:25:31 +0900

    변경

--- generate.py
+++ generate.py
@@ -1,4 +1,9 @@
 import pyrosim.pyrosim as pyrosim
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+for i in range(5):
+    for j in range(5):
+        for k in range(10) :
+            sizefactor = [0.8**k, 0.8**k, 0.8**k]
+            pyrosim.Send_Cube(pos=[i-2.5, j-2.5, k+0.5], size=sizefactor)
 pyrosim.End()
+
--- simulation.py
+++ simulation.py
@@ -1,5 +1,10 @@
 import pybullet as p
+import pybullet_data
 physicsclient =p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+
 p.loadSDF("box.sdf")
 import time
 for i in range(1000) :
