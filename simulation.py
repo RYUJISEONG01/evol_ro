1bc9157b77f88e4530ec60ed54a3394367a5c989
RYUJISEONG01 <wsd2380@naver.com>
2025-01-15 16:21:20 +0900

    변경

---
+++ simulation.py
@@ -0,0 +1,9 @@
+import pybullet as p
+physicsclient =p.connect(p.GUI)
+p.loadSDF("box.sdf")
+import time
+for i in range(1000) :
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
+p.disconnect()
