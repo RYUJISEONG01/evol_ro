73393bce39a6a5339ab739a9d659227f087d561f
RYUJISEONG01 <wsd2380@naver.com>
2025-01-16 11:30:58 +0900

    변경

--- simulation.py
+++ simulation.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
 import pybullet_data
+import numpy
+import os
 physicsclient =p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
@@ -10,10 +12,17 @@ robotId=p.loadURDF("body.urdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
 import time
-for i in range(1000) :
+LlegSV=numpy.zeros(10000)
+
+for i in range(500) :
     p.stepSimulation()
-    LLeg_touchsensor=pyrosim.Get_Touch_Sensor_Value_For_Link("LLeg")
-    print(f"sensor value :{LLeg_touchsensor}"  )
+    LlegSV[i]=pyrosim.Get_Touch_Sensor_Value_For_Link("LLeg")
+
     time.sleep(1/60)
-    print(i)
+
 p.disconnect()
+data_directory="data"
+os.makedirs(data_directory, exist_ok=True)
+output_filename = os.path.join(data_directory, "LlegSV.npy")
+numpy.save(output_filename,LlegSV)
+print(f"sensor values saved to {output_filename}")
